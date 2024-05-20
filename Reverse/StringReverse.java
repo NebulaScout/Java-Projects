0f11c5ad0a8ba23da5d4b98ce4705ddab2b2e3d3
NebulaScout <ribak427@gmail.com>
2024-05-20 15:05:04 +0300

    Reversing a string.

---
+++ Reverse/StringReverse.java
@@ -0,0 +1,22 @@
+// A program that reverses a string input  and prints the output
+
+package Reverse;
+import java.util.Scanner;
+
+public class StringReverse {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Input some data");  // get user input
+        String userInput = scan.nextLine();
+        scan.close();
+
+        // Convert the string to an array
+        char[] input = userInput.toCharArray();
+
+        // Traverse the array
+        for(int i = input.length; i > 0; i--) {
+            System.out.print(input[i -1]); // print output in reverse
+        } // end of for loop
+        System.out.println();
+    } // end of main method
+}
