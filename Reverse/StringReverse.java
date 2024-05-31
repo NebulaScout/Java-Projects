717b09ced86ff2a9d4614fbe79090a0ea9c69e4d
NebulaScout <ribak427@gmail.com>
2024-05-31 13:53:08 +0300

    Loop through the program to allow for continuous input.

--- Reverse/StringReverse.java
+++ Reverse/StringReverse.java
@@ -2,23 +2,35 @@
 
 package Reverse;
 import java.util.Scanner;
+// import java.util.Thread;
 
 public class StringReverse {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Input some data");  // get user input
-        String userInput = scan.nextLine();
+        boolean reverse = true;
+
+        while(reverse) {
+            System.out.println("Input some data:[Leave blank and press Enter to exit the program]");  // get user input
+            String userInput = scan.nextLine();
+
+            if(userInput.isBlank()) {
+                reverse = false;
+                System.out.println("Exiting the program.");
+                System.exit((0));
+            }
+
+            // Convert the string to an array
+            char[] input = userInput.toCharArray();
+
+            System.out.println("\nInput string: " + userInput);
+            System.out.print("Reversed String: ");
+            
+            // Traverse the array
+            for(int i = input.length; i > 0; i--) {
+                System.out.print(input[i -1]); // print output in reverse
+            } // end of for loop
+            System.out.println("\n");
+        }
         scan.close();
-
-        // Convert the string to an array
-        char[] input = userInput.toCharArray();
-
-        System.out.println("\nInput string: " + userInput);
-        System.out.print("Reversed String: ");
-        // Traverse the array
-        for(int i = input.length; i > 0; i--) {
-            System.out.print(input[i -1]); // print output in reverse
-        } // end of for loop
-        System.out.println();
     } // end of main method
 }
