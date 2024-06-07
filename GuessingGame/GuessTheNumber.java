55bb718e8e7b796fa9deaecf31aaf2dc289c70b7
NebulaScout <ribak427@gmail.com>
2024-06-07 16:00:06 +0300

    Provide information on how the game works.

--- GuessingGame/GuessTheNumber.java
+++ GuessingGame/GuessTheNumber.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 public class GuessTheNumber {
     // driver method
     public static void main(String[] args) {
-        // TODO: Tell the user how the game works
             printDetails();
     }
 
@@ -12,11 +11,11 @@ public class GuessTheNumber {
     public static String[] guessNum() {
         int randomNum = (int)(Math.ceil(Math.random() * 100));  // generate the random number
         byte count = 0;
-        byte maxNumOfGuesses = 5;
+        byte maxNumOfGuesses = 6;
         boolean correct = false;
         String[] gameStats = new String[3];   // store the game stats
 
-        System.out.println(randomNum);
+        // System.out.println(randomNum);
         Scanner scan = new Scanner(System.in);
         while(count <= maxNumOfGuesses) {
             // exit the game if the user has used up all of his/her chances
@@ -31,7 +30,7 @@ public class GuessTheNumber {
             // check if the user has guessed the correct number
             // exit the game if the guessed number is correct
             if(userGuess == randomNum) {
-                System.out.println("Correct\n");
+                System.out.println("Spot on!\n");
                 scan.close();
                 correct = true;
                 gameStats[0] = Boolean.toString(correct);
@@ -57,21 +56,41 @@ public class GuessTheNumber {
 
     }
 
+    // print the details
     public static void printDetails() {
+        String blank = "";
+        String heading = "Welcome to the Number Guessing Game!\n";
+        String intro = String.format("%130s", blank).replace(" ", "-"); // format the output
+        // tell the user about the game and how to play
+        System.out.println(intro + "\n\n"
+                            + String.format("%80s", heading) + "\n"
+                            + intro
+                            + "\n\nIn this exciting game, the program will randomly generate a secret number between 1 and 100.\nYour mission is to guess this number.\n\n"
+                            + "Here's how it works:\n\n"
+                            + "\t- You have 6 attempts to guess the correct number.\n"
+                            + "\t- After each guess, the program will tell you if your guess is too high, too low or spot on!\n"
+                            + "\t- Use the feedback to refine your next guess and try to pinpoint the secret number.\n"
+                            + "\nThink you can outsmart the computer? Give it a try and see if you can guess the number within 6 attempts!\n"
+                            + "\nGood luck and have fun!\n\n"
+                            + intro + "\n");
+
+        // TODO: Add press enter to start feature
+
         String[] stats = guessNum();
         String result;
 
+        // check if the user guessed the right number
         if (stats[0] == "true")
-            result = "Congratulations, you won the game 🏆 🎊";
+            result = "Congratulations, you won the game! 🏆🎊";
         else
-            result = "Better luck next time 🥹";
-
-        String blank = "";
-        String heading = "Game Stats";
-        String hyphens = String.format("%50s", blank).replace(" ", "-");
-        System.out.println(hyphens + "\n" 
-                            + String.format("%32s", heading) + "\n"
-                            + hyphens + "\n"
+        result = "You’ve used all your chances 🥹. Better luck next time!";
+    
+        // print the stats of the game
+        String headingStats = String.format("%60s", blank).replace(" ", "-");
+        heading = "Game Stats";
+        System.out.println(headingStats + "\n\n" 
+                            + String.format("%32s", heading) + "\n\n"
+                            + headingStats + "\n\n"
                             + result + "\n\n"
                             + "Number of guesses: " + stats[1] + "\n"
                             + "The correct number is " + stats[2] + "\n");
